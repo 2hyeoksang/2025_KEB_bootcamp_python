76d311803edb6423d706ecbb9cad108089fddef8
LeeSH <tkdgurdhkd99@naver.com>
2025-02-11 13:31:46 +0900

    v0.6 prime function

--- day03.py
+++ day03.py
@@ -13,13 +13,35 @@ num = int(input("수를 입력하시오."))
 # else:
 #     print("소수입니다.")
 
-t1 = True
-for i in range(2,int(math.sqrt(num)+1)):
-    if num % i == 0 :
-        t1 = False
-        break
+# t1 = True
+# for i in range(2,int(math.sqrt(num)+1)):
+#     if num % i == 0 :
+#         t1 = False
+#         break
+#
+# if t1 == False or num == 1:
+#     print("합성수입니다.")
+# else:
+#     print("소수입니다.")
 
-if t1 == False or num == 1:
-    print("합성수입니다.")
+def is_prime(num) -> bool :
+    '''
+    소수 판별
+    :param num: integer number
+    :return: bool type
+    '''
+    if num >= 2:
+        for i in range(2,int(math.sqrt(num)+1)):
+            if num % i == 0 :
+                return False
+
+    else :
+        return False
+    return True
+
+if is_prime(num):
+    print("Prime")
 else:
-    print("소수입니다.")
+    print("NOT Prime")
+
+help(is_prime)
