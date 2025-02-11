972bcb9b396db38802d495194f2121faaeb842a7
LeeSH <tkdgurdhkd99@naver.com>
2025-02-11 16:08:29 +0900

    v0.9 exercise

--- day03.py
+++ day03.py
@@ -1,47 +1,40 @@
-import math
-
-num = int(input("수를 입력하시오."))
-
-# t1 = 0
-# for i in range(2,num):
-#     if num % i == 0 :
-#         t1 = 1
-#         break
+# #university = r"Inha\nUniversity" # r : 보이는 그대로 찍는
+# university = "Inha\nUniversity"
+# #print(university)
 #
-# if t1 == 1 or num == 1 :
-#     print("합성수입니다.")
-# else:
-#     print("소수입니다.")
-
-# t1 = True
-# for i in range(2,int(math.sqrt(num)+1)):
-#     if num % i == 0 :
-#         t1 = False
-#         break
+# print(university[:-11])
+# print(university[::2])
 #
-# if t1 == False or num == 1:
-#     print("합성수입니다.")
-# else:
-#     print("소수입니다.")
+# subjects = ["python", "c++", "database"]
+# subjects_string = " / ".join(subjects)
+# print(subjects_string)
 
-def is_prime(num) -> bool :
-    '''
-    소수 판별
-    :param num: integer number
-    :return: bool type
-    '''
-    if num >= 2:
-        for i in range(2,int(math.sqrt(num)+1)):
-            if num % i == 0 :
-                return False
+# course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
+# print(course.find('KEB'))
+# print(course.rfind('KEB')) # 뒤에서 찾는거
+# print(course.index('KEB'))
+# print(course.rindex('KEB'))
+# print(course.find('Inha'))  # -1 find 와 index의 차이 : find는 없으면 -1 return, index는 없으면 에러
+# #print(course.index('Inha'))  # ValueError: substring not found
+# print(course)
+# course = course.replace('KEB', 'Inha', 3)
+# print(course)
+# print(course.strip())
+# print(course.strip("!#.*"))
+# print(course.lstrip("!#*.")) # 혹은 rstrip
 
-    else :
-        return False
-    return True
+# print(course)
+# print(course.replace('KEB', 'Inha'))
+# print(course)
+# course = course.replace('KEB', 'Inha')
+# print(course)
 
-if is_prime(num):
-    print("Prime")
-else:
-    print("NOT Prime")
-
-help(is_prime)
+# preview
+subjects = "python c++ database linux"
+print('%e' % 0.7045)
+print(subjects.isalnum()) # al : alphabet
+subject = input("수강신청과목 입력 : ")
+try:
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
+except ValueError:
+    print('해당과목이 존재하지 않습니다')
