afeb35058f75cc056cd5c4520964936f03ac3895
LeeSH <tkdgurdhkd99@naver.com>
2025-02-12 17:47:11 +0900

    day3 HW

--- day04.py
+++ day04.py
@@ -76,43 +76,68 @@
 #         print(f'다음에 또 오세요')
 #         break
 
-# import random
-# drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
-# # print(drinks_foods)
-# # print(drinks_foods.pop("고량주"))
-# # print(drinks_foods)
-#
-# #del drinks_foods["위스키"]
-# #drinks_foods["사케"] = "광어회"
-# japan_drinks_foods = {"사케": "광어회", "위스키": "낙곱새"}
-# drinks_foods.update(japan_drinks_foods)
-#
-# #drink = input(drinks_foods.keys())
-# drinks_foods_keys = list(drinks_foods)
-# # print(drinks_foods_keys)
-# # #print(drinks_foods_keys.pop(0))
-# # print(drinks_foods_keys.remove("위스키"))
-# # print(drinks_foods_keys)
-# #print(random.choice(drinks_foods_keys))
-#
-# while True:
-#     menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_keys[0]}   2) {drinks_foods_keys[1]}   3) {drinks_foods_keys[2]}   4) {drinks_foods_keys[3]}   5) {drinks_foods_keys[4]}   6) 아무거나   7) 종료 : ')
-#     if menu == '1':
-#         print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
-#     elif menu == '2':
-#         print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다')
-#     elif menu == '3':
-#         print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다')
-#     elif menu == '4':
-#         print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
-#     elif menu == '5':
-#         print(f'{drinks_foods_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[4]]} 입니다')
-#     elif menu == '6':
-#         random_drink = random.choice(drinks_foods_keys)
-#         print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
-#     elif menu == '7':
-#         print(f'다음에 또 오세요')
-#         break
+import random
+
+drinks=['위스키', '와인', '소주', '고량주','데킬라']
+foods = ['초콜릿', '치즈', '삼겹살', '양꼬치','라임']
+
+japan_drinks = ['사케', '위스키']
+japan_foods = ['광어회', '낙곱새']
+
+for drink, food in zip(japan_drinks, japan_foods) :
+    if drink in drinks :
+        foods[drinks.index(drink)] = food
+    else:
+        drinks.append(drink)
+        foods.append(food)
+
+# print(drinks)
+# print(foods)
+
+menu_list = '다음 술 중에 고르시오.\n'
+for i in range(len(drinks)):
+    menu_list = menu_list + f'{i+1}) {drinks[i]}     '
+menu_list = menu_list + f'{len(drinks)+1}) 아무거나     {len(drinks)+2}) 종료 : '
+
+
+def print_menu(menu):
+    try :
+        print(f'{drinks[menu - 1]}에 어울리는 안주는 {foods[menu - 1]} 입니다.')
+    except IndexError :
+        print("없는 메뉴입니다.")
+
+
+while True:
+    try:
+        menu = int(input(menu_list))
+        if menu == len(drinks)+2:
+            print('다음에 또 오세요.')
+            break
+        elif menu == len(drinks)+1:
+            random_idx = random.randint(1, len(drinks))
+            print_menu(random_idx)
+        else :
+            print_menu(menu)
+        print()
+    except :
+        print()
+    # if menu == '1':
+    #     print(f'{drinks[0]}에 어울리는 안주는 {foods[0]} 입니다')
+    # elif menu == '2':
+    #     print(f'{drinks[1]}에 어울리는 안주는 {foods[1]} 입니다')
+    # elif menu == '3':
+    #     print(f'{drinks[2]}에 어울리는 안주는 {foods[2]} 입니다')
+    # elif menu == '4':
+    #     print(f'{drinks[3]}에 어울리는 안주는 {foods[3]} 입니다')
+    # elif menu == '5':
+    #     print(f'{drinks[4]}에 어울리는 안주는 {foods[4]} 입니다')
+    # elif menu == '6':
+    #     random_idx = random.randint(0, len(drinks)-1)
+    #     print(f'{drinks[random_idx]}에 어울리는 안주는 {foods[random_idx]} 입니다')
+    # elif menu == '7':
+    #     print(f'다음에 또 오세요')
+    #     break
+
 
 # import random
 # star = ['테란', '저그', '프로토스']
