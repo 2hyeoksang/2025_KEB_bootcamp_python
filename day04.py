4f8c3a7fa28edcf58476418d17fe4e65a001a7e3
LeeSH <tkdgurdhkd99@naver.com>
2025-02-13 16:25:10 +0900

    v3.6 assignment

--- day04.py
+++ day04.py
@@ -1,25 +1,18 @@
-# def out_func(nout):
-#     def inner_func():
-#         return nout * nout
-#     return inner_func
-#
-#
-# x = out_func(9)
-# print(type(x))
-# print(x)
-# print(x())
 import time
-
-
-# nums=['7', '-11', '3']
-#
-# for i in range(len(nums)):
-#     nums[i] = int(nums[i])
-#
-# print(nums)
+# v3.6) 2중 데코레이터 적용. 성능측정 데코레이터, 디스크립션 데코레이터를 팩토리얼 함수에 적용하시오.
 
 #SOLID
 #Open Closed Principal : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀있는 원칙)
+
+def description_decorator(f):
+    def inner(*arg):
+        print(f.__name__)
+        print(f.__doc__)
+        r = f(*arg)
+        return r
+    return inner
+
+
 def time_decorator(func):
     def wrapper(*arg):
         s = time.time()
@@ -29,19 +22,20 @@ def time_decorator(func):
         return r
     return wrapper
 
-#@time_decorator
+
+@description_decorator
+@time_decorator
 def factorial(n):
+    """
+    calculating the value of factorial of n
+    """
     result = 1
     for i in range(2,n+1):
         result = result * i
     return result
 
-number = int(input('num : '))
-# s = time.time()
-ft = time_decorator(factorial)
-print(f"{number}! = {ft(number)}")
 
 number=int(input("num : "))
+# f1 = description_decorator(factorial)
+# print(f1(number))
 print(f'{number}! = {factorial(number)}')
-# e = time.time()
-# print(e-s)
